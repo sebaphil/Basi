f2c9f3e8562290f911c5155bce762d8bd3f1ced2
SandroBac <baccegasandro@gmail.com>
2016-12-14 09:58:30 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,20 +8,9 @@
 <body>
 	<div id="container">
 	 
-	    <div id="header">
-	    	Appunti universitari 
-	        <div id="navigation">
-	        	<a href="">Login</a>
-	        	<a href="">Sign Up</a>
-	        	<div id="tfheader">
-					<form id="tfnewsearch" method="get" action="http://www.google.com">
-				    	<input type="text" class="tftextinput" name="q" size="21" maxlength="120"><input type="submit" value="search" class="tfbutton">
-					</form>
-					<div class="tfclear"></div>
-				</div>
-	        </div><!--#navigation-->
-	         
-	    </div><!--#header-->
+	    <?php
+		import("header.php");
+	    ?>
 	 
 	        <div id="main">
 	        	<!--#percorso-->
