a0e42626c4e51d2a47a8f7f6d6230a11accfed82
Fundor333 <fundor333@gmail.com>
2016-12-14 17:11:50 +0100

    Cleaning the logout

--- logout.php
+++ logout.php
@@ -1,5 +1,5 @@
-<?php session_start(); ?>
-<?php require 'function.php'; ?>
 <?php
+session_start();
+require 'function.php';
 logout();
 header('Location: ' . "http://$_SERVER[HTTP_HOST]" . '/index.php');
