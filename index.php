c8f806411629c70e49423e47a5e7b2bd47047611
sebaphil <seba.fil@live.com>
2016-12-14 09:25:43 +0100

    Initial push.

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Appunti universitari</title>
+</head>
+<body>
+	<div id="container">
+	 
+	    <div id="header">
+	     
+	        <div id="navigation"></div><!--#navigation-->
+	         
+	    </div><!--#header-->
+	 
+	        <div id="main"></div><!--#main-->
+	 
+	        <div id="sidebar"></div><!--#sidebar-->
+	     
+	    <div id="footer"></div><!--#footer-->
+	 
+	</div><!--#container-->
+</body>
+</html>
