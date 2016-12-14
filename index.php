7dbca6b39b8595408449fcf1552d46bd1f93d8c7
sebaphil <seba.fil@live.com>
2016-12-14 09:39:42 +0100

    Update css e index

--- index.php
+++ index.php
@@ -2,13 +2,24 @@
 <html>
 <head>
 	<title>Appunti universitari</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+
 </head>
 <body>
 	<div id="container">
 	 
 	    <div id="header">
-	     
-	        <div id="navigation"></div><!--#navigation-->
+	    	Appunti universitari 
+	        <div id="navigation">
+	        	<a href="">Login</a>
+	        	<a href="">Sign Up</a>
+	        	<div id="tfheader">
+					<form id="tfnewsearch" method="get" action="http://www.google.com">
+				    	<input type="text" class="tftextinput" name="q" size="21" maxlength="120"><input type="submit" value="search" class="tfbutton">
+					</form>
+					<div class="tfclear"></div>
+				</div>
+	        </div><!--#navigation-->
 	         
 	    </div><!--#header-->
 	 
