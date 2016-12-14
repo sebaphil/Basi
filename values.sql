ed25802eb6e131adc0732d9df46144ce33785cd0
sebaphil <seba.fil@live.com>
2016-12-14 16:25:22 +0100

    Added values.sql

---
+++ values.sql
@@ -0,0 +1,8 @@
+INSERT INTO Utenti (Username, Password, Nome, Cognome, AnnoNascita, Email, Tipo) VALUES
+    ('User1', 'pw1', 'Nome1','Cognome1', '01/01/01','email1','0'),
+    ('User2', 'pw2', 'Nome2','Cognome2', '02/02/02','email2','0'),
+    ('User3', 'pw3', 'Nome3','Cognome3', '03/03/03','email3','0'),
+    ('User4', 'pw4', 'Nome4','Cognome4', '04/04/04','email4','0'),
+    ('User5', 'pw5', 'Nome5','Cognome5', '05/05/05','email5','0'),
+    ('User6', 'pw6', 'Nome6','Cognome6', '06/06/06','email6','0');
+
