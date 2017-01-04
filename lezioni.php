8c65db23d547639362b5ce2a0796e6b86e20d74d
sebaphil <seba.fil@live.com>
2017-01-04 15:58:21 +0100

    Added files

--- lezioni.php
+++ lezioni.php
@@ -6,10 +6,16 @@ require_once "head.php";
 echo "<title>".$titolo_pagina."</title>"
 ?>
 <body>
+<?php
+        if(isset($_GET['corso'])){ $column = $_GET['corso'];}
 
-    <?php
+        $sql = "SELECT * FROM lezioni INNER JOIN corsi on lezioni.Corso=corsi.Codice WHERE lezioni.Corso=";
+        if(!empty($column)){
+            $sql."'".$column."';";
+        }
 
-        $result = pg_query($dbconn, "SELECT * FROM corsi;");
+
+        $result = pg_query($dbconn, $sql);
         if (!$result) {
             echo "An error occurred.\n";
             exit;
@@ -19,14 +25,10 @@ echo "<title>".$titolo_pagina."</title>"
         echo "<table align='center'>";
         while ($row = pg_fetch_row($result)) {
             echo "<tr>";
-            echo "<td><a href='/lezioni.php?corso=".$row[0]."'>".$row[1]."</a></td>";
+            echo "<td><a href='/appunti.php?lezione=".$row[0]."'>".$row[1]." Docente: ".$row[3]."</a></td>";
         }
         echo "</table>";
 
     ?>
-
 </body>
-<?php
-require_once "foot.php";
-?>
 </html>
